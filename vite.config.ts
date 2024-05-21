e3309bfaf6158a95f206a0353c9af2a3dd21cab2
eosedelnikova <eosedelnikova@luxoft.com>
2024-05-21 14:05:09 +0200

    MS-3 - using TS

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-/// <reference types="vitest" />
-
-import { URL, fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import analyzer from 'rollup-plugin-analyzer'
 import { defineConfig } from 'vite'
+import * as path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      src: fileURLToPath(new URL('src', import.meta.url))
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@services': path.resolve(__dirname, 'src/services')
     }
   },
   server: {
@@ -20,26 +18,5 @@ export default defineConfig({
       overlay: true
     }
   },
-  plugins: [vue(), analyzer({ summaryOnly: true })],
-  test: {
-    environment: 'happy-dom',
-    setupFiles: './src/setupTests.ts',
-    globals: true,
-    snapshotFormat: {
-      escapeString: false
-    },
-    coverage: {
-      enabled: true,
-      provider: 'v8',
-      include: ['src'],
-      exclude: [
-        'src/*.{ts,vue}',
-        'src/services/api.ts',
-        'src/setupTests.ts',
-        'src/utils/test',
-        '**/*.d.ts'
-      ],
-      all: true
-    }
-  }
+  plugins: [vue(), analyzer({ summaryOnly: true })]
 })
